fcefa89a58e4a81b70ffb2b3187b0af0cf4f7c5f
lfy55 <sqk_55@163.com>
2017-11-15 10:09:51 +0800

    add globe page

---
+++ globe/scripts/timeline.js
@@ -0,0 +1,7 @@
+
+function startTimeline() {
+  var slider = $("#timeline-point"),
+    labels = $("#timeline .timeline-label>div"),
+    maxLeft = $("#timeline-line").width() - slider.width();
+  console.log(maxLeft)
+}
