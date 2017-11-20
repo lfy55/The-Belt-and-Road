95658ea3e23e756a019ecaf30e3d7a747bfb9913
SHI-Hui <shihuihs@163.com>
2017-11-20 17:03:19 +0800

    radar shihui

--- map/scripts/detail.js
+++ map/scripts/detail.js
@@ -2,4 +2,7 @@ lineChart.initChart({
   dataY: [300, 150, 100, 250, 50, 330, 150, 400, 300, 330, 250, 400, 420, 450, 480, 520]
 });
 
-radarChart.initChart();
+radarChart.initChart({
+  data1:[100, 8, 0.40, -180, 2000],
+  data2:[60, 5, 0.30, -100, 1500]
+});
